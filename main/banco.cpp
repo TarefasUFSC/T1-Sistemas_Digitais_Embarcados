a5acdc257bfa567bc8c8cf021cafd41cee7a6fb3
ManoellaR <manu.rockemba@gmail.com>
2022-11-12 17:57:58 -0300

    a

--- main/banco.cpp
+++ main/banco.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "banco.hpp"
+#include "serial.h"
 
 void Banco::resetBanco(){
 
@@ -18,10 +19,16 @@ Registro Banco::getRegistro(int add){
 }
 
 int Banco::getIndiceRegistroPorNome(){
+    printf("Digite o nome: ");
+    serial.readString((uint8_t *)leitura,20);
+    printf("%s\n",leitura);
 
 }
 
 int Banco::getIndiceRegistroPorTelefone(){
+    printf("Digite o telefone: ");
+    serial.readString((uint8_t *)leitura,10);
+    printf("%s\n",leitura);
 
 }
 
